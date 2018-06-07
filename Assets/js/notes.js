3eaf7b7386262de78a97f7661ad0a96a47493cfc
Luis <salazarluis0119@gmail.com>
2018-06-06 19:27:34 -0500

    go

---
+++ Assets/js/notes.js
@@ -0,0 +1,54 @@
+//notes for myself
+// nothing is linked, this is just a notes file.
+
+const PETS = ["Dog", "Cat", "Danger"];
+
+// cannot change. 
+
+// can't change PETS. 
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// use 'let' to declare variables which will change
+let time = 10;
+
+// this is fine
+time = 12;
+console.log(time)
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// this below would throw an error. 
+let answer = 42
+
+let answer = 7 * 6;
+// this above would throw an error. 
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//var does not care. 
+var answer = 1;
+var answer = 2;
+//var does not care.
+
+
+
+//* Remember, both `const` and `let` are block scoped.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
+
+
+
+
+
